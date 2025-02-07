f714cddeeec1ca326ee6fff525dbb5317300ab5b
Angel <bernamezchok@gmail.com>
2025-02-07 09:41:39 -0600

    copilot

--- app/Http/Controllers/blog/BlogController.php
+++ app/Http/Controllers/blog/BlogController.php
@@ -4,20 +4,28 @@ namespace App\Http\Controllers\Blog;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
     public function index()
     {
-        $posts = Post::paginate(2); // O usa paginate() si deseas paginación
-        
-        return view('blog.index', compact('posts'));
+        $posts = Post::all();
+        return view('blog.index', ['posts' => $posts]);
     }
+
     public function show($id)
     {
         $post = Post::findOrFail($id);
-        return view('blog.detail', ['post' => $post]);
-    }
+        $categories = Category::all();
+        $users = User::all();
 
+        return view('blog.detail', [
+            'post' => $post,
+            'categories' => $categories,
+            'users' => $users
+        ]);
+    }
 }
--- resources/views/blog/detail.blade.php
+++ resources/views/blog/detail.blade.php
@@ -1,6 +1,25 @@
-<div class="card card-white -mt-2">    
-    <h1>{{$post->awebo}}</h1>
-     <span>{{$post->title}}</span>
+@extends('blog.master')
 
-  <hr>
-   {{$post->content}} 
+@section('content')
+<div class="card card-white -mt-2">    
+    <h1>{{ $post->title }}</h1>
+    <span>{{ $post->title }}</span>
+    <hr>
+    <div>{{ $post->content }}</div>
+</div>
+
+<h2>Categories</h2>
+<ul class="list-group">
+    @foreach ($categories as $category)
+        <li class="list-group-item">{{ $category->name }}</li>
+    @endforeach
+</ul>
+
+<h2>Users</h2>
+<ul class="list-group">
+    @foreach ($users as $user)
+        <li class="list-group-item">
+            <strong>{{ $user->name }}</strong> ({{ $user->email }})
+        </li>
+    @endforeach
+</ul>
