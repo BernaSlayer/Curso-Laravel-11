6ffa2a17e1291cc0d6f134da664b7288fab7bbf0
Angel <bernamezchok@gmail.com>
2025-02-11 11:23:11 -0600

    fix

---
+++ resources/js/vue/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1> Principal</h1>
+        <list/>
+    </div>
+</template>
+
+<script>
+    
+    import List from './components/ListComponents.vue';
+    
+    export default {
+        components: {
+            List
+        }
+    }
+    
+
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue'; // 👈 Importa el plugin de Vue
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/vue/main.js' // 👈 Asegúrate de incluir tu archivo Vue
+            ],
             refresh: true,
         }),
+        vue(), // 👈 Agrega el plugin de Vue
     ],
 });
